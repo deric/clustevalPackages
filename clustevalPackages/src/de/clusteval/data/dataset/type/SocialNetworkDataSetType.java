4eae4dfc149c386157ecb44ff24a93856496adde
wiwiec <derwiwie@googlemail.com>
2014-05-10 14:27:19 +0200

    added two dataset types

---
+++ clustevalPackages/src/de/clusteval/data/dataset/type/SocialNetworkDataSetType.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Wiwie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Christian Wiwie - initial API and implementation
+ ******************************************************************************/
+/**
+ * 
+ */
+package de.clusteval.data.dataset.type;
+
+import java.io.File;
+
+import de.clusteval.framework.repository.RegisterException;
+import de.clusteval.framework.repository.Repository;
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+public class SocialNetworkDataSetType extends DataSetType {
+
+	/**
+	 * @param repository
+	 * @param register
+	 * @param changeDate
+	 * @param absPath
+	 * @throws RegisterException
+	 * 
+	 */
+	public SocialNetworkDataSetType(final Repository repository,
+			final boolean register, final long changeDate, final File absPath)
+			throws RegisterException {
+		super(repository, register, changeDate, absPath);
+	}
+
+	/**
+	 * The copy constructor for this type.
+	 * 
+	 * @param other
+	 *            The object to clone.
+	 * @throws RegisterException
+	 */
+	public SocialNetworkDataSetType(final SocialNetworkDataSetType other)
+			throws RegisterException {
+		super(other);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see data.dataset.type.DataSetType#getAlias()
+	 */
+	@Override
+	public String getAlias() {
+		return "Social Network";
+	}
+
+}
