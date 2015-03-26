903dd5c363295b569b52b38e9cc1e4fd2347f02f
wiwiec <derwiwie@googlemail.com>
2015-03-26 11:08:05 +0100

    * added sample normalizers (variance and min-max) * added pPAM clusterin gmethod

---
+++ clustevalPackages/src/de/clusteval/data/preprocessing/SampleRangeNormalizationDataPreprocessor.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Wiwie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Christian Wiwie - initial API and implementation
+ ******************************************************************************/
+/**
+ * 
+ */
+package de.clusteval.data.preprocessing;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.Rserve.RserveException;
+
+import de.clusteval.data.dataset.AbsoluteDataSet;
+import de.clusteval.data.dataset.DataMatrix;
+import de.clusteval.data.dataset.DataSet;
+import de.clusteval.data.dataset.RelativeDataSet;
+import de.clusteval.data.dataset.format.InvalidDataSetFormatVersionException;
+import de.clusteval.data.dataset.format.UnknownDataSetFormatException;
+import de.clusteval.framework.repository.MyRengine;
+import de.clusteval.framework.repository.RegisterException;
+import de.clusteval.framework.repository.Repository;
+
+/**
+ * Normalize every feature of the dataset (columns of the data matrix) to values
+ * between 0 and 1.
+ * 
+ * @author Christian Wiwie
+ * 
+ */
+public class SampleRangeNormalizationDataPreprocessor extends DataPreprocessor {
+
+	/**
+	 * @param repository
+	 * @param register
+	 * @param changeDate
+	 * @param absPath
+	 * @throws RegisterException
+	 */
+	public SampleRangeNormalizationDataPreprocessor(Repository repository,
+			boolean register, long changeDate, File absPath)
+			throws RegisterException {
+		super(repository, register, changeDate, absPath);
+	}
+
+	/**
+	 * @param other
+	 * @throws RegisterException
+	 */
+	public SampleRangeNormalizationDataPreprocessor(
+			SampleRangeNormalizationDataPreprocessor other)
+			throws RegisterException {
+		super(other);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.clusteval.data.preprocessing.DataPreprocessor#preprocess(de.clusteval
+	 * .data.DataConfig)
+	 */
+	@Override
+	public DataSet preprocess(DataSet data) {
+		if (data instanceof RelativeDataSet)
+			throw new IllegalArgumentException(
+					"The range normalization is only applicable to absolute coordinates");
+		final AbsoluteDataSet dataSet = (AbsoluteDataSet) data;
+		try {
+			dataSet.loadIntoMemory();
+			DataMatrix matrix = dataSet.getDataSetContent();
+			try {
+				MyRengine rEngine = repository.getRengineForCurrentThread();
+				try {
+					rEngine.assign("x", matrix.getData());
+					rEngine.eval("x.norm <- x - apply(x,MARGIN=1,min)");
+					rEngine.eval("x.norm <- x.norm / apply(x.norm,MARGIN=1,max)");
+					double[][] result = rEngine.eval("x.norm").asDoubleMatrix();
+
+					DataMatrix newMatrix = new DataMatrix(matrix.getIds(),
+							result);
+					DataSet newDataSet = dataSet.clone();
+					newDataSet.setAbsolutePath(new File(dataSet
+							.getAbsolutePath() + ".sampleRangeNorm"));
+					newDataSet.setDataSetContent(newMatrix);
+					newDataSet.writeToFile(false);
+					newDataSet.unloadFromMemory();
+					return newDataSet;
+				} catch (REngineException e) {
+					e.printStackTrace();
+				} catch (REXPMismatchException e) {
+					e.printStackTrace();
+				} finally {
+					rEngine.clear();
+				}
+			} catch (RserveException e) {
+				e.printStackTrace();
+			}
+		} catch (InvalidDataSetFormatVersionException e1) {
+			e1.printStackTrace();
+		} catch (IllegalArgumentException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (UnknownDataSetFormatException e) {
+			e.printStackTrace();
+		} finally {
+			dataSet.unloadFromMemory();
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.clusteval.data.preprocessing.DataPreprocessor#getCompatibleDataSetFormats
+	 * ()
+	 */
+	@Override
+	public Set<String> getCompatibleDataSetFormats() {
+		return new HashSet<String>(
+				Arrays.asList(new String[]{"MatrixDataSetFormat"}));
+	}
+}
---
+++ clustevalPackages/src/de/clusteval/program/r/PPAMClusteringRProgram.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Wiwie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Christian Wiwie - initial API and implementation
+ ******************************************************************************/
+/**
+ * 
+ */
+package de.clusteval.program.r;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.Rserve.RserveException;
+
+import de.clusteval.cluster.Clustering;
+import de.clusteval.context.Context;
+import de.clusteval.context.UnknownContextException;
+import de.clusteval.data.dataset.format.DataSetFormat;
+import de.clusteval.data.dataset.format.UnknownDataSetFormatException;
+import de.clusteval.framework.RLibraryRequirement;
+import de.clusteval.framework.repository.RegisterException;
+import de.clusteval.framework.repository.Repository;
+import de.clusteval.program.Program;
+import de.clusteval.run.result.format.RunResultFormat;
+import de.clusteval.run.result.format.UnknownRunResultFormatException;
+import file.FileUtils;
+
+/**
+ * This class is a parallized implementation of K-Medoids Clustering based on
+ * the R-framework function ppam() of the sprint package.
+ * 
+ * @author Christian Wiwie
+ * 
+ */
+@RLibraryRequirement(requiredRLibraries = {"sprint"})
+public class PPAMClusteringRProgram extends RelativeDataRProgram {
+
+	/**
+	 * @param repository
+	 * @throws RegisterException
+	 */
+	public PPAMClusteringRProgram(Repository repository)
+			throws RegisterException {
+		super(repository, new File(FileUtils.buildPath(
+				repository.getBasePath(Program.class),
+				"PPAMClusteringRProgram.jar")).lastModified(), new File(
+				FileUtils.buildPath(repository.getBasePath(Program.class),
+						"PPAMClusteringRProgram.jar")));
+	}
+
+	/**
+	 * The copy constructor of K-Medoids clustering.
+	 * 
+	 * @param other
+	 *            The object to clone.
+	 * 
+	 * @throws RegisterException
+	 */
+	public PPAMClusteringRProgram(PPAMClusteringRProgram other)
+			throws RegisterException {
+		this(other.repository);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see program.Program#getAlias()
+	 */
+	@Override
+	public String getAlias() {
+		return "k-Medoids (pPAM)";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see program.r.RProgram#getInvocationFormat()
+	 */
+	@Override
+	public String getInvocationFormat() {
+		return "ppam(x,k=%k%)";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.clusteval.program.r.RProgram#getClusterIdsFromExecResult()
+	 */
+	@Override
+	protected float[][] getFuzzyCoeffMatrixFromExecResult()
+			throws RserveException, REXPMismatchException {
+		REXP result = rEngine.eval("result$clustering");
+		int[] clusterIds = result.asIntegers();
+		return Clustering.clusterIdsToFuzzyCoeff(clusterIds);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.clusteval.program.r.RProgram#getCompatibleDataSetFormats()
+	 */
+	@Override
+	public Set<DataSetFormat> getCompatibleDataSetFormats()
+			throws UnknownDataSetFormatException {
+		return new HashSet<DataSetFormat>(DataSetFormat.parseFromString(
+				repository, new String[]{"SimMatrixDataSetFormat"}));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.clusteval.program.r.RProgram#getRunResultFormat()
+	 */
+	@Override
+	public RunResultFormat getRunResultFormat()
+			throws UnknownRunResultFormatException {
+		return RunResultFormat.parseFromString(repository,
+				"TabSeparatedRunResultFormat");
+	}
+
+	@Override
+	public Context getContext() throws UnknownContextException {
+		return Context.parseFromString(repository, "ClusteringContext");
+	}
+}
