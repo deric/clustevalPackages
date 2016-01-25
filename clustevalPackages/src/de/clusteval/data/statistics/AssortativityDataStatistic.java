fc56d59a2f05ec79daedb00cd5e817e2f6d29e9d
Mikkel <Mikkel@mikkelHansen>
2016-01-25 13:52:37 +0100

    Added assortativity data statistic

---
+++ clustevalPackages/src/de/clusteval/data/statistics/AssortativityDataStatistic.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Wiwie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Christian Wiwie - initial API and implementation
+ ******************************************************************************/
+/**
+ * 
+ */
+package de.clusteval.data.statistics;
+
+import java.io.File;
+
+import de.clusteval.framework.RLibraryRequirement;
+import de.clusteval.framework.repository.RegisterException;
+import de.clusteval.framework.repository.Repository;
+
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+@RLibraryRequirement(requiredRLibraries = {"igraph"})
+public class AssortativityDataStatistic
+		extends
+			DoubleValueDataStatistic {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see utils.Statistic#getAlias()
+	 */
+	@Override
+	public String getAlias() {
+		return "Assortativity";
+	}
+
+	/**
+	 * @param repository
+	 * @param register
+	 * @param changeDate
+	 * @param absPath
+	 * @throws RegisterException
+	 * 
+	 */
+	public AssortativityDataStatistic(final Repository repository,
+			final boolean register, final long changeDate, final File absPath)
+			throws RegisterException {
+		super(repository, register, changeDate, absPath, 0.0);
+	}
+
+	/**
+	 * @param repository
+	 * @param register
+	 * @param changeDate
+	 * @param absPath
+	 * @param value
+	 * @throws RegisterException
+	 */
+	public AssortativityDataStatistic(final Repository repository,
+			final boolean register, final long changeDate, final File absPath,
+			final double value) throws RegisterException {
+		super(repository, register, changeDate, absPath, value);
+	}
+
+	/**
+	 * The copy constructor for this statistic.
+	 * 
+	 * @param other
+	 *            The object to clone.
+	 * @throws RegisterException
+	 */
+	public AssortativityDataStatistic(
+			final AssortativityDataStatistic other)
+			throws RegisterException {
+		super(other);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see data.statistics.DataStatistic#requiresGoldStandard()
+	 */
+	@Override
+	public boolean requiresGoldStandard() {
+		return false;
+	}
+
+}
---
+++ clustevalPackages/src/de/clusteval/data/statistics/AssortativityDataStatisticCalculator.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Wiwie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Christian Wiwie - initial API and implementation
+ ******************************************************************************/
+/**
+ * 
+ */
+package de.clusteval.data.statistics;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+
+import de.wiwie.wiutils.utils.ArraysExt;
+import de.wiwie.wiutils.utils.SimilarityMatrix;
+import de.clusteval.data.DataConfig;
+import de.clusteval.data.dataset.DataSetConfig;
+import de.clusteval.data.dataset.RelativeDataSet;
+import de.clusteval.data.dataset.format.InvalidDataSetFormatVersionException;
+import de.clusteval.data.dataset.format.UnknownDataSetFormatException;
+import de.clusteval.framework.repository.MyRengine;
+import de.clusteval.framework.repository.RegisterException;
+import de.clusteval.framework.repository.Repository;
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+public class AssortativityDataStatisticCalculator
+		extends
+			DataStatisticRCalculator<AssortativityDataStatistic> {
+
+	/**
+	 * @param repository
+	 * @param changeDate
+	 * @param absPath
+	 * @param dataConfig
+	 * @throws RegisterException
+	 */
+	public AssortativityDataStatisticCalculator(Repository repository,
+			long changeDate, File absPath, DataConfig dataConfig)
+			throws RegisterException {
+		super(repository, changeDate, absPath, dataConfig);
+	}
+
+	/**
+	 * The copy constructor for this statistic calculator.
+	 * 
+	 * @param other
+	 *            The object to clone.
+	 * @throws RegisterException
+	 */
+	public AssortativityDataStatisticCalculator(
+			final AssortativityDataStatisticCalculator other)
+			throws RegisterException {
+		super(other);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see data.statistics.DataStatisticCalculator#calculate()
+	 */
+	@Override
+	protected AssortativityDataStatistic calculateResultHelper(
+			final MyRengine rEngine) throws IllegalArgumentException,
+			IOException, InvalidDataSetFormatVersionException,
+			RegisterException, REngineException, REXPMismatchException,
+			UnknownDataSetFormatException, InterruptedException {
+
+		DataSetConfig dataSetConfig = dataConfig.getDatasetConfig();
+		RelativeDataSet dataSet = (RelativeDataSet) (dataSetConfig.getDataSet()
+				.getInStandardFormat());
+
+		if (!dataSet.isInMemory())
+			dataSet.loadIntoMemory();
+		SimilarityMatrix simMatrix = dataSet.getDataSetContent();
+		if (dataSet.isInMemory())
+			dataSet.unloadFromMemory();
+
+		double[][] similarities = simMatrix.toArray();
+		similarities = ArraysExt.scaleBy(similarities,
+				ArraysExt.max(similarities), true);
+
+		rEngine.assign("simMatrix", similarities);
+		rEngine.eval("library('igraph')");
+		rEngine.eval("gr <- graph.adjacency(simMatrix,weighted=TRUE)");
+		rEngine.eval("gr <- simplify(as.undirected(gr,mode='collapse'), remove.loops=TRUE, remove.multiple=TRUE)");
+		REXP result = rEngine.eval("assortativity_degree(gr, directed = FALSE)");
+		return new AssortativityDataStatistic(repository, false,
+				changeDate, absPath, result.asDouble());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see data.statistics.DataStatisticCalculator#writeOutputTo(java.io.File)
+	 */
+	@SuppressWarnings("unused")
+	@Override
+	protected void writeOutputToHelper(File absFolderPath,
+			final MyRengine rEngine) {
+	}
+
+}
